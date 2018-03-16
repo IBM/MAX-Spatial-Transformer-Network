21cd3cdf1e0f2ff193d00da8626e6ceedbe3b487
Luciano Resende <lresende@us.ibm.com>
2018-03-16 11:02:55 -0700

    Enable retrieving data from shared COS

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@ if [ ! -f ./training-runs.yml ]; then
     sed -i .bak "s/    access_key_id:.*/    access_key_id: $AWS_ACCESS_KEY_ID/g" training-runs.yml
     sed -i .bak "s/    secret_access_key:.*/    secret_access_key: $AWS_SECRET_ACCESS_KEY/g" training-runs.yml
 
-    #curl -O https://github.com/daviddao/spatial-transformer-tensorflow/raw/master/data/mnist_sequence1_sample_5distortions5x5.npz
+    curl -O http://max-assets.s3-api.us-geo.objectstorage.softlayer.net/mnist_sequence1_sample_5distortions5x5.npz
 
     aws --endpoint-url=http://s3-api.us-geo.objectstorage.softlayer.net s3 mb s3://spatial-transformer-training
     aws --endpoint-url=http://s3-api.us-geo.objectstorage.softlayer.net s3 cp mnist_sequence1_sample_5distortions5x5.npz s3://spatial-transformer-training/data/
